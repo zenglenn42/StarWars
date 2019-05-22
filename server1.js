e678c84a26059a6d4427a47f13733932cf642939
zenglenn42 <gsnospampls@pobox.com>
2019-05-22 18:50:49 -0500

    update port

--- server1.js
+++ server1.js
@@ -3,7 +3,7 @@
 var express = require("express");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Data
 // ===========================================================
